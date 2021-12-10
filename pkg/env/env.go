f007d5ca85c6ba0f96ecbae10d3842a5ac37606b
Jose Sitanggang <josealfredositanggang@gmail.com>
2021-12-10 21:17:21 +0700

    create env package with fallback value

---
+++ pkg/env/env.go
@@ -0,0 +1,65 @@
+package env
+
+import (
+	"os"
+	"strconv"
+	"time"
+)
+
+// String returns the env string value if the key exists.
+// Otherwise, returns initial value.
+func String(key, initial string) string {
+	v, exists := os.LookupEnv(key)
+	if !exists {
+		return initial
+	}
+	return v
+}
+
+// Int returns the env integer value if the key exists.
+// Otherwise, returns initial value.
+func Int(key string, initial int) int {
+	v := String(key, "")
+	if v == "" {
+		return initial
+	}
+
+	n, err := strconv.Atoi(v)
+	if err != nil {
+		panic(err)
+	}
+
+	return n
+}
+
+// Duration returns the env duration value if the key exists.
+// Otherwise, returns initial value.
+func Duration(key string, initial time.Duration) time.Duration {
+	v := String(key, "")
+	if v == "" {
+		return initial
+	}
+
+	d, err := time.ParseDuration(v)
+	if err != nil {
+		panic(err)
+	}
+
+	return d
+}
+
+// Bool returns the env boolean value if the key exists.
+// Otherwise, returns initial value.
+func Bool(key string, initial bool) bool {
+	v := String(key, "")
+	if v == "" {
+		return initial
+	}
+
+	b, err := strconv.ParseBool(v)
+	if err != nil {
+		panic(err)
+	}
+
+	return b
+}
